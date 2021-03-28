2064b3a3f27523020171a6203f193e0e4207a70d
Elijah Mazaev <42232971+inthemiddleofthenight@users.noreply.github.com>
2021-03-28 09:53:18 +0300

    DataBulkManager.MSSQL (add async method) [assembly: AssemblyVersion("1.0.1")] -> [assembly: AssemblyVersion("1.0.3")] clean solution

--- DataBulkManager.MSSQL/Bulk.cs
+++ DataBulkManager.MSSQL/Bulk.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
+using System.Threading.Tasks;
 
 namespace DataBulkManager.MSSQL
 {
@@ -56,6 +57,49 @@ namespace DataBulkManager.MSSQL
             catch (Exception ex)
             {
                 throw new DbmException($"{nameof(DataBulkManager.MSSQL.Bulk)} {nameof(Insert)}<{typeof(T).Name}> error", ex);
+            }
+        }
+
+        public async Task InsertAsync<T>(IEnumerable<T> entities,
+          SqlBulkCopyOptions sqlBulkCopyOptions = SqlBulkCopyOptions.Default,
+          int timeout = 30,
+          bool enableStreaming = false,
+          int batchSize = 0,
+          SqlTransaction sqlTransaction = null) where T : class
+        {
+            try
+            {
+                SqlBulkCopy sqlBulkCopy = new SqlBulkCopy(_sqlConnection, sqlBulkCopyOptions, sqlTransaction)
+                {
+                    DestinationTableName = typeof(T).GetBulkTableName(),
+                    BulkCopyTimeout = timeout,
+                    EnableStreaming = enableStreaming,
+                    BatchSize = batchSize
+                };
+
+                DataTable dataTable = new DataTable();
+
+                var attributes = typeof(T).GetBulkColumnAttributes();
+
+                foreach (BulkColumnAttribute bulkColumnAttribute in attributes)
+                {
+                    dataTable.Columns.Add(new DataColumn(bulkColumnAttribute.Name, bulkColumnAttribute.Type));
+                    sqlBulkCopy.ColumnMappings.Add(bulkColumnAttribute.Name, bulkColumnAttribute.Name);
+                }
+
+                foreach (T entity in entities)
+                {
+                    dataTable.Rows.Add(entity.GetBulkEntityValues());
+                }
+
+                if (_sqlConnection.State != ConnectionState.Open)
+                    _sqlConnection.Open();
+
+                await sqlBulkCopy.WriteToServerAsync(dataTable);
+            }
+            catch (Exception ex)
+            {
+                throw new DbmException($"{nameof(DataBulkManager.MSSQL.Bulk)} {nameof(InsertAsync)}<{typeof(T).Name}> error", ex);
             }
         }
     }
